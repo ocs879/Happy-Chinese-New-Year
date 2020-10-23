5a8458fd263f74fe6383bc61ba299ce4493ab572
ocs879 <70246675+ocs879@users.noreply.github.com>
2020-10-23 10:24:06 +0800

    Update Assignment.js

--- Assignment.js
+++ Assignment.js
@@ -1,5 +1,5 @@
 function sendtowhatsapp(){
     alert("We are leaving this site. Press OK to proceed.");
-    window.open('https://wa.me/?text=https://ocs879.github.io/Assignment_Page1.html');
+    window.open('http://wa.me/?text=https://ocs879.github.io/Assignment_Page1.html');
 }
 
