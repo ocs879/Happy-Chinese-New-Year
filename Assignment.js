971d6f5fc38f78be506fc6854214b3e2271d7a48
ocs879 <70246675+ocs879@users.noreply.github.com>
2020-10-24 16:41:47 +0800

    Change code.

--- Assignment.js
+++ Assignment.js
@@ -1,5 +1,8 @@
 function sendtowhatsapp(){
-    alert("We are leaving this site. Press OK to proceed.");
-    window.open('http://wa.me/?text=https://ocs879.github.io/Assignment_Page1.html');
+    var r = confirm("We are leaving this site. Press OK to proceed.");
+    if (r == true) {
+      window.open("http://wa.me/?text=https://ocs879.github.io/Assignment_Page1.html");
+    } else {
+      window.close("http://wa.me/?text=https://ocs879.github.io/Assignment_Page1.html");
+    }
 }
-
