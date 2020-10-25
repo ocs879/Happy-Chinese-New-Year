af55d8c7d92427390c5db2d6e7926060977a7599
ocs879 <70246675+ocs879@users.noreply.github.com>
2020-10-25 15:57:17 +0800

    Update Assignment.js

--- Assignment.js
+++ Assignment.js
@@ -1,8 +1,8 @@
 function sendtowhatsapp(){
     var r = confirm("We are leaving this site. Press OK to proceed.");
     if (r == true) {
-      window.open("http://wa.me/?text=https://ocs879.github.io/Assignment_Page1.html");
+      window.open("http://wa.me/?text=https://ocs879.github.io/CNY_Greet.html");
     } else {
-      window.close("http://wa.me/?text=https://ocs879.github.io/Assignment_Page1.html");
+      window.close("http://wa.me/?text=https://ocs879.github.io/CNY_Greet.html");
     }
 }
