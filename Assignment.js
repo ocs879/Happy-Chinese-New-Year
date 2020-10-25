a532331fddd8701cfcf3a18c34b4a9abf791e063
ocs879 <70246675+ocs879@users.noreply.github.com>
2020-10-25 18:37:08 +0800

    Update Assignment.js

--- Assignment.js
+++ Assignment.js
@@ -15,3 +15,12 @@ function sendtowhatsapp_no2(){
     window.close("http://wa.me/?text=https://ocs879.github.io/Christmas_Greet.html");
   }
 }
+
+function sendtowhatsapp_no3(){
+  var r = confirm("We are leaving this site. Press OK to proceed.");
+  if (r == true) {
+    window.open("http://wa.me/?text=https://ocs879.github.io/HariRaya_Greet.html");
+  } else {
+    window.close("http://wa.me/?text=https://ocs879.github.io/HariRaya_Greet.html");
+  }
+}
