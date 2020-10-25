dd60b0c36cc616cf3775787b4da196fa96ffc17b
ocs879 <70246675+ocs879@users.noreply.github.com>
2020-10-25 19:29:17 +0800

    Update Assignment.js

--- Assignment.js
+++ Assignment.js
@@ -7,6 +7,15 @@ function sendtowhatsapp(){
     }
 }
 
+function sendtowhatsapp_no1(){
+  var r = confirm("We are leaving this site. Press OK to proceed.");
+  if (r == true) {
+    window.open("http://wa.me/?text=https://ocs879.github.io/Homepage.html");
+  } else {
+    window.close("http://wa.me/?text=https://ocs879.github.io/Homepage.html");
+  }
+}
+
 function sendtowhatsapp_no2(){
   var r = confirm("We are leaving this site. Press OK to proceed.");
   if (r == true) {
@@ -22,5 +31,14 @@ function sendtowhatsapp_no3(){
     window.open("http://wa.me/?text=https://ocs879.github.io/HariRaya_Greet.html");
   } else {
     window.close("http://wa.me/?text=https://ocs879.github.io/HariRaya_Greet.html");
+  }
+}
+
+function sendtowhatsapp_no4(){
+  var r = confirm("We are leaving this site. Press OK to proceed.");
+  if (r == true) {
+    window.open("http://wa.me/?text=https://ocs879.github.io/Deepavali_Greet.html");
+  } else {
+    window.close("http://wa.me/?text=https://ocs879.github.io/Deepavali_Greet.html");
   }
 }
